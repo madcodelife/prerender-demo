e7c622de7da03b324e77b8ab6030489dfd23ed3b
choujiaojiao <gassnake999@163.com>
2019-08-23 10:46:36 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
-  render: h => h(App)
+  render: h => h(App),
+  mounted () {
+    // You'll need this for renderAfterDocumentEvent.
+    document.dispatchEvent(new Event('render-event'))
+  }
 }).$mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  configureWebpack: () => {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        plugins: [
+          new PrerenderSPAPlugin({
+            staticDir: resolve('dist'),
+            routes: ['/', '/about'],
+            renderer: new Renderer({
+              inject: {
+                _m: 'prerender'
+              },
+              headless: true,
+              renderAfterDocumentEvent: 'render-event'
+            })
+          })
+        ]
+      }
+    }
+  }
+}
